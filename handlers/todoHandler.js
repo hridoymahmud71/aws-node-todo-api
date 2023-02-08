11f24bdec18e754f660fb666a5db7a76d9eb8bb8
Mahmudur Rahman <hridoymahmud71@gmail.com>
2023-02-08 11:50:32 +0600

    list working

--- handlers/todoHandler.js
+++ handlers/todoHandler.js
@@ -4,12 +4,33 @@ const {
     DynamoDBDocumentClient,
     GetCommand,
     PutCommand,
+    ScanCommand
 } = require("@aws-sdk/lib-dynamodb");
 
 
+const marshallOptions = {
+    // Whether to automatically convert empty strings, blobs, and sets to `null`.
+    convertEmptyValues: false, // false, by default.
+    // Whether to remove undefined values while marshalling.
+    removeUndefinedValues: false, // false, by default.
+    // Whether to convert typeof object to map attribute.
+    convertClassInstanceToMap: false, // false, by default.
+};
+
+const unmarshallOptions = {
+    // Whether to return numbers as a string instead of converting them to native JavaScript numbers.
+    wrapNumbers: false, // false, by default.
+};
+
+const translateConfig = { marshallOptions, unmarshallOptions };
+
+
+
 const TODO_TABLE = process.env.TODO_TABLE;
-const client = new DynamoDBClient();
-const dynamoDb = DynamoDBDocumentClient.from(client);
+const client = new DynamoDBClient({});
+const ddbDocClient = DynamoDBDocumentClient.from(client, translateConfig);
+
+
 
 
 
@@ -22,4 +43,30 @@ const createTodo = async (event, context) => {
 
 }
 
-module.exports = { createTodo }
+const listTodo = async (event, context, callback) => {
+
+    try {
+
+        const response = await ddbDocClient.send(new ScanCommand({ TableName: TODO_TABLE }));
+
+
+        console.log(response);
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ response })
+        }
+
+    } catch (error) {
+        console.log("what error: ",error)
+        return {
+            statusCode: 500,
+            body: JSON.stringify(error)
+        }
+    }
+
+
+
+}
+
+module.exports = { createTodo, listTodo }
